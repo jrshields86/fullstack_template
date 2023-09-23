7a69b933c1bd0faf6def711a40b9eb2ddb244e9a
jrshields86 <jrshields86@gmail.com>
2023-09-23 09:30:16 -0600

    data in browser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios'
 
 const App = ()=> {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const {data} = await axios.get('/api/movies')
+      console.log(data)
+    }
+    fetchMovies()
+  }, []);
   return (
-    <h1>My Movies</h1>
+    <div>
+      <h1>My Movies</h1>
+
+    </div>
   );
 };
 
